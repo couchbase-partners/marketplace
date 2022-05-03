4c06929f5aeff301182df23037334234bf2d5988
Justin Ashworth <justin.ashworth@couchbase.com>
2022-05-03 18:53:53 -0400

    * Fix issues with ARM Template Validation to provide better feedback when failing * Fix issues with Couchbase Server template to address issues found by Template validation

--- azure/wrapper.sh
+++ azure/wrapper.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-pwsh -noprofile -nologo -command "Import-Module '/arm-ttk/arm-ttk/arm-ttk.psd1'; Test-AzTemplate template/ ; if (\$Error.Count) { exit 1}"
+pwsh -noprofile -nologo -command "Import-Module '/arm-ttk/arm-ttk/arm-ttk.psd1'; Test-AzTemplate template/ -Skip Allowed-Values-Should-Actually-Be-Allowed ; if (\$Error.Count) { exit 1}"
