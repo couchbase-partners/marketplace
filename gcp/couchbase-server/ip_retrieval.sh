5a6d24bc1c01629e9604b08d6494122aa59e2f3a
Justin Ashworth <justin.ashworth@couchbase.com>
2022-09-16 17:06:53 -0400

    More fixes for gcp

--- gcp/couchbase-server/ip_retrieval.sh
+++ gcp/couchbase-server/ip_retrieval.sh
@@ -8,7 +8,7 @@ do
         *) exit 1;;
     esac
 done
-
+gcloud components install beta --quiet > /dev/null 2>&1
 RUNTIME_CONFIG=$(gcloud deployment-manager deployments describe "$STACK_NAME" --format=json --quiet | jq -r '.outputs[] | select(.name=="runtimeConfigName") | .finalValue')
 EXTERNALIP=$(gcloud beta runtime-config configs variables describe ExternalIp --config-name="$RUNTIME_CONFIG" --format=json --quiet | jq -r '.value')
 
